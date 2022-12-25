68420fefaa246c95471276345eaac92c0ed92800
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-25 12:29:43 +0200

    Connect to the database

--- server/index.js
+++ server/index.js
@@ -2,3 +2,30 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
+
+const app = express();
+dotenv.config();
+mongoose.set('strictQuery', false);
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.CONNECTION_URL);
+        console.log('Connected to mongoDB.')
+    } catch (error) {
+        throw error;
+    }
+};
+
+mongoose.connection.on('disconnected', () => {
+    console.log('mongoDB disconnected!');
+});
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+    connect();
+    console.log("Connected to backend.");
+});
