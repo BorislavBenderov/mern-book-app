8ba37a5b15d061d95b115eabc0ee902e16494802
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-26 17:39:26 +0200

    Add updatePost in posts controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
 export const getPosts = async (req, res) => {
@@ -22,4 +23,17 @@ export const createPost = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message });
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body;
+
+    if (mongoose.Types.isValid(_id)) {
+        return res.status(404).send('No post with that id!');
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatedPost);
 }
