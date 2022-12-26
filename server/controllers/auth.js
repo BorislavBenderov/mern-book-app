35388eb540a9c76fcdc8b1aae87bf84b1b1d51df
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-26 20:36:12 +0200

    Add login in auth controller

---
+++ server/controllers/auth.js
@@ -0,0 +1,28 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import User from '../models/user.js';
+
+
+export const login = async (req, res) => {
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) return res.status(404).json({ message: 'User not found!' });
+
+        const isPasswordCorrect = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+
+        if (!isPasswordCorrect) return res.status(400).json({ message: 'Invalid credentials' });
+
+        const token = jwt.sign(
+            { id: user._id, email: user.email },
+            process.env.JWT
+        );
+
+        res.status(200).json({ result: user, token });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong!' });
+    }
+};
