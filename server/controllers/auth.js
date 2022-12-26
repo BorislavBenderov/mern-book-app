e57c94a3c29d786d0940b9f6046e11842656a9df
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-26 20:46:25 +0200

    Add register in auth controller

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,6 +2,30 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/user.js';
 
+export const register = async (req, res) => {
+    try {
+        const { email, password, confirmPassword } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (user) return res.status(400).json({ message: 'User already exists!' });
+
+        if (password !== confirmPassword) return res.status(400).json({ message: 'Password do not match!' })
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        const result = await User.create({ email, password: hash });
+        const token = jwt.sign(
+            { id: result._id, email: result.email },
+            process.env.JWT
+        );
+
+        res.status(200).json({ result, token });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong!' });
+    }
+};
 
 export const login = async (req, res) => {
     try {
