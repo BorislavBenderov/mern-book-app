793781358c3f3b4d5fa7f8d04add6d4afd87e3c4
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-27 14:06:01 +0200

    Add Auth Context

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,20 @@ import { Header } from "./components/header/Header";
 import { Dashboard } from "./pages/dashboard/Dashboard";
 import { Login } from './pages/login/Login';
 import { Routes, Route } from 'react-router-dom';
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <div className='max-w-6xl m-auto'>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-      <Footer />
-    </div>
+    <AuthContextProvider>
+      <div className='max-w-6xl m-auto'>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </div>
+    </AuthContextProvider>
   );
 }
 
---
+++ client/src/contexts/AuthContext.js
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+    const [loggedUser, setLoggedUser] = useLocalStorage('profile', {});
+
+    const userLogin = (result) => {
+        setLoggedUser(result);
+    }
+
+    const userLogout = (result) => {
+        setLoggedUser({});
+    }
+
+    return (
+        <AuthContext.Provider value={{ loggedUser, userLogin, userLogout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
