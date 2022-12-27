d72d79fbb33402b00497b9c781dc9776e4712022
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-27 12:18:44 +0200

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
 import { Dashboard } from "./pages/dashboard/Dashboard";
 import { Login } from './pages/login/Login';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className='max-w-6xl m-auto'>
       <Header />
-      <Login />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <Footer />
     </div>
   );
