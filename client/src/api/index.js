d6461f7b79974bbbc2999c588a7eaa5f7f85fc79
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-27 14:05:35 +0200

    Add api service to make requests

---
+++ client/src/api/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+API.interceptors.request.use((req) => {
+    if (localStorage.getItem('profile')) {
+        req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+        console.log(req.headers.authorization);
+    }
+
+    return req;
+});
+
+export const fetchPosts = () => API.get('/posts');
+export const createPost = (newPost) => API.post('/posts', newPost);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
+export const updatePost = (id, updatedPost) => API.patch(`/posts${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts${id}`);
+
+export const login = (authData) => API.post('/auth/login', authData);
+export const register = (authData) => API.post('/auth/register', authData);
