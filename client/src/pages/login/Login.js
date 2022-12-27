de128f7e4c42f68d5ae4522f30da05adc712a86d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-27 14:07:16 +0200

    Add login page and possibility users to login

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,6 +1,35 @@
+import { login } from "../../api";
+import { useNavigate } from 'react-router-dom';
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+
 export const Login = () => {
+    const { userLogin } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const onLogin = (e) => {
+        e.preventDefault();
+
+        const authData = Object.fromEntries(new FormData(e.target));
+
+        if (authData.email === '' || authData.password === '') {
+            alert('Please fill all the fields!');
+            return;
+        }
+
+        login(authData)
+            .then((req) => {
+                userLogin(req.data);
+                navigate('/');
+            })
+            .catch((err) => {
+                console.log(err);
+                alert(err.message);
+            })
+    }
     return (
-        <form className='w-80 bg-blue-400 rounded-lg py-12 px-8 m-auto mt-20'>
+        <form
+            className='w-80 bg-blue-400 rounded-lg py-12 px-8 m-auto mt-20'
+            onSubmit={onLogin}>
             <h3 className='text-2xl font-medium leading-10 text-center'>Login Here</h3>
             <label
                 className='block mt-8 text-base font-medium'
