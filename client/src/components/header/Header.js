712b6d87990c014a60f96d244a4cc9938a19c1ce
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-27 14:08:02 +0200

    Show/hide buttons for logged and guest users and add possibility users to logout

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,6 +1,14 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
 
 export const Header = () => {
+    const { loggedUser, userLogout } = useContext(AuthContext);
+
+    const onLogout = () => {
+        userLogout();
+    }
+
     return (
         <header className="">
             <div className="h-20 flex justify-between items-center">
@@ -10,23 +18,26 @@ export const Header = () => {
                     </Link>
                 </div>
                 <div className="">
-                    <>
-                        <Link to="/login" className="ml-5">
-                            <span>Login</span>
-                        </Link>
-                        <Link to="/register" className="ml-5">
-                            <span>Register</span>
-                        </Link> </>
-                    <> <span></span>
-                        <Link to="/mymovies" className="ml-5">
-                            <span>My Books</span>
-                        </Link>
-                        <Link to="/create" className="ml-5">
-                            <span>Create Book</span>
-                        </Link>
-                        <Link to="" className="ml-5">
-                            <span>Logout</span>
-                        </Link> </>
+                    {!loggedUser.result?._id
+                        ? <>
+                            <Link to="/login" className="ml-5">
+                                <span>Login</span>
+                            </Link>
+                            <Link to="/register" className="ml-5">
+                                <span>Register</span>
+                            </Link>
+                        </>
+                        : <> <span></span>
+                            <Link to="/mymovies" className="ml-5">
+                                <span>My Books</span>
+                            </Link>
+                            <Link to="/create" className="ml-5">
+                                <span>Create Book</span>
+                            </Link>
+                            <Link to="" className="ml-5" onClick={onLogout}>
+                                <span>Logout</span>
+                            </Link>
+                        </>}
                 </div>
             </div>
         </header>
