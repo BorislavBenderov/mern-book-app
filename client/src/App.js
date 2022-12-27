1d147c969958b62e7e87fa3a5f9b09be9abffb17
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-27 16:47:49 +0200

    Add CreateBook page and possibility users to create books

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Login } from './pages/login/Login';
 import { Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from "./contexts/AuthContext";
 import { Register } from "./pages/register/Register";
+import { CreateBook } from "./pages/create-book/CreateBook";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/create" element={<CreateBook />} />
         </Routes>
         <Footer />
       </div>
---
+++ client/src/pages/create-book/CreateBook.js
@@ -0,0 +1,80 @@
+import { createPost } from "../../api";
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+export const CreateBook = () => {
+    const { loggedUser } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const onCreate = (e) => {
+        e.preventDefault();
+
+        const formData = Object.fromEntries(new FormData(e.target));
+
+        if (formData.title === '' || formData.description === '' || formData.image === '' || formData.type === '') {
+            alert('Please fill all the fields!');
+            return;
+        }
+
+        const bookData = {
+            ...formData,
+            ownerId: loggedUser.result._id
+        };
+
+        createPost(bookData)
+            .then(() => {
+                navigate('/');
+            })
+            .catch((err) => {
+                alert(err.message);
+            })
+    }
+    return (
+        <form
+            className='w-80 bg-blue-400 rounded-lg py-12 px-8 m-auto mt-20'
+            onSubmit={onCreate}>
+            <h3 className='text-2xl font-medium leading-10 text-center'>Create New Book</h3>
+            <label
+                className='block mt-8 text-base font-medium'
+                htmlFor="title">Title</label>
+            <input
+                className='block h-12 w-full bg-indigo-400 rounded-sm px-3 mt-2 text-sm'
+                type="text"
+                placeholder="Title"
+                id="title"
+                name="title" />
+            <label
+                className='block mt-8 text-base font-medium'
+                htmlFor="description">Description</label>
+            <input
+                className='block h-12 w-full bg-indigo-400 rounded-sm px-3 mt-2 text-sm'
+                type="text"
+                placeholder="Description"
+                id="description"
+                name="description" />
+            <label
+                className='block mt-8 text-base font-medium'
+                htmlFor="image">Image Url</label>
+            <input
+                className='block h-12 w-full bg-indigo-400 rounded-sm px-3 mt-2 text-sm'
+                type="text"
+                placeholder="Image Url"
+                id="image"
+                name="image" />
+            <label
+                className='block mt-8 text-base font-medium'
+                htmlFor="type">Type</label>
+            <input
+                className='block h-12 w-full bg-indigo-400 rounded-sm px-3 mt-2 text-sm'
+                type="text"
+                placeholder="Type"
+                id="type"
+                name="type" />
+            <button
+                className='mt-12 w-full bg-white py-4 text-base font-semibold rounded cursor-pointer text-blue-600'
+                type="submit">Create</button>
+            <p className=""></p>
+        </form>
+    );
+}
