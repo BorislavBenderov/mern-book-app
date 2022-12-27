6ef4626f30f4b231681818246a81001afea8bacd
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-27 12:19:18 +0200

    Add link from react router for header buttons

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,30 +1,32 @@
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
     return (
         <header className="">
             <div className="h-20 flex justify-between items-center">
                 <div className="">
-                    <a href="/" className="">
+                    <Link to="/" className="">
                         Home
-                    </a>
+                    </Link>
                 </div>
                 <div className="">
                     <>
-                        <a href="/login" className="ml-5">
+                        <Link to="/login" className="ml-5">
                             <span>Login</span>
-                        </a>
-                        <a href="/register" className="ml-5">
+                        </Link>
+                        <Link to="/register" className="ml-5">
                             <span>Register</span>
-                        </a> </>
+                        </Link> </>
                     <> <span></span>
-                        <a href="/mymovies" className="ml-5">
+                        <Link to="/mymovies" className="ml-5">
                             <span>My Books</span>
-                        </a>
-                        <a href="/create" className="ml-5">
+                        </Link>
+                        <Link to="/create" className="ml-5">
                             <span>Create Book</span>
-                        </a>
-                        <a href="" className="ml-5">
+                        </Link>
+                        <Link to="" className="ml-5">
                             <span>Logout</span>
-                        </a> </>
+                        </Link> </>
                 </div>
             </div>
         </header>
