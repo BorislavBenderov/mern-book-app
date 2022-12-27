ef3d1134b1f67ff9797f37249ffab0fc9cfc57b3
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-27 11:45:24 +0200

    Update footer css

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,6 +1,6 @@
 export const Footer = () => {
     return (
-        <footer className="text-center mb-4">
+        <footer className="text-center mb-4 mt-8">
             <p>© 2022 - Borislav Benderov - All Rights Reserved</p>
         </footer>
     );
