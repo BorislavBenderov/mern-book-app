c845063fb30b5db3dea8cf34c582a377c52905c4
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-27 18:46:35 +0200

    Add Link to Book Card page

--- client/src/components/book-card/BookCard.js
+++ client/src/components/book-card/BookCard.js
@@ -1,19 +1,25 @@
-export const BookCard = ({book}) => {
+import { Link } from 'react-router-dom';
+
+export const BookCard = ({ book }) => {
     return (
-        <div className="">
-            <div className="mb-2 overflow-hidden">
-                <img
-                    src={book.image}
-                    alt="book"
-                    className="rounded-2xl"
-                />
-            </div>
-            <div className="">
-                <h3 className="text-xl font-semibold">{book.title}</h3>
+        <>
+            <Link to={`/books/${book._id}`}>
                 <div className="">
-                    <span className="">{book.description}</span>
+                    <div className="mb-2 overflow-hidden">
+                        <img
+                            src={book.image}
+                            alt="book"
+                            className="rounded-2xl"
+                        />
+                    </div>
+                    <div className="">
+                        <h3 className="text-xl font-semibold">{book.title}</h3>
+                        <div className="">
+                            <span className="">{book.description}</span>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </Link>
+        </>
     );
 }
