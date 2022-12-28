1ea99413f6983233557adb9a6cd04fbec4cda5a7
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-28 09:05:30 +0200

    Fix delete post request

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -15,7 +15,7 @@ export const fetchPosts = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts', newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
-export const deletePost = (id) => API.delete(`/posts${id}`);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
 
 export const login = (authData) => API.post('/auth/login', authData);
 export const register = (authData) => API.post('/auth/register', authData);
