545cef80abe3036477bde991c1efca8cde5afee9
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-28 11:59:37 +0200

    Add LikeBook component and possibility users to like books

---
+++ client/src/components/like-book/LikeBook.js
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { likePost } from "../../api";
+import { AuthContext } from "../../contexts/AuthContext";
+import { BookContext } from "../../contexts/BookContext";
+
+export const LikeBook = ({ book }) => {
+    const { loggedUser } = useContext(AuthContext);
+    const { onLikeBook } = useContext(BookContext);
+    console.log(book);
+
+    const onLike = () => {
+        likePost(book._id)
+            .then(() => {
+                onLikeBook(loggedUser?.result._id, book._id)
+            })
+            .catch((err) => {
+                alert(err.message);
+            })
+    }
+
+    return (
+        <i className={`fa fa-heart${!book?.likes?.includes(loggedUser?.result._id) ? '-o' : ''} fa-lg`}
+            style={{ cursor: 'pointer', color: book?.likes?.includes(loggedUser?.result._id) ? 'red' : null }}
+            onClick={onLike}
+        ></i>
+    );
+}
