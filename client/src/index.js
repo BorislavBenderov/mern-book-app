05c7918f3c96c4fa97dfd01fe58870626beff42c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-28 11:59:06 +0200

    Add font awesome

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import "font-awesome/css/font-awesome.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
