15e8a11271d1d39bd433b8aa0f339ef301c0262b
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-28 17:36:32 +0200

    Render 404 page when path is not found

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import { CreateBook } from "./pages/create-book/CreateBook";
 import { EditBook } from './pages/edit-book/EditBook';
 import { BookContextProvider } from "./contexts/BookContext";
 import { BookDetails } from "./pages/book-details/BookDetails";
+import { NotFound } from "./pages/not-found/NotFound";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <div className='max-w-6xl m-auto'>
           <Header />
           <Routes>
+            <Route path='*' element={<NotFound />} />
             <Route path="/" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
