ec6ad53ee1b30d813ff6fb7d52f8510ac6a2bc86
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-29 10:38:15 +0200

    Add route to commentPost

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, deletePost, likePost, commentPost, deleteCommentPost } from '../controllers/posts.js';
 import auth from '../middlewares/auth.js';
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', auth, commentPost);
+
 
 export default router;
