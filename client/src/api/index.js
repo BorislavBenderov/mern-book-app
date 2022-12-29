6e8f762d1e09d8707749718b45c9f11c519a3d32
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-29 10:39:29 +0200

    Add commentPost in fetch actions

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -16,6 +16,7 @@ export const createPost = (newPost) => API.post('/posts', newPost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const commentPost = (comment, id) => API.post(`/posts/${id}/commentPost`, { comment });
 
 export const login = (authData) => API.post('/auth/login', authData);
 export const register = (authData) => API.post('/auth/register', authData);
