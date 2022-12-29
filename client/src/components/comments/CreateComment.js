fca95fa5de0af8366ce2667a61674ac4d57974ec
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-29 14:08:56 +0200

    Add CreateComment component and possibility users to create comments

---
+++ client/src/components/comments/CreateComment.js
@@ -0,0 +1,45 @@
+import { useContext, useState } from "react";
+import { commentPost } from "../../api";
+import { BookContext } from "../../contexts/BookContext";
+import { AuthContext } from "../../contexts/AuthContext";
+
+export const CreateComment = ({ bookId }) => {
+    const [comment, setComment] = useState('');
+    const { onCommentBook } = useContext(BookContext);
+    const { loggedUser } = useContext(AuthContext);
+    const ownerName = loggedUser?.result?.email;
+
+    const commentData = {
+        comment,
+        ownerName
+    };
+
+    const onComment = (e) => {
+        e.preventDefault();
+
+        commentPost(commentData, bookId)
+            .then((res) => {
+                onCommentBook(res.data.comments, bookId);
+                setComment('');
+            })
+            .catch((err) => {
+                alert(err.message);
+            })
+    }
+
+    return (
+        <form className="flex" onSubmit={onComment}>
+            <label htmlFor="comment" />
+            <textarea
+                className="resize-none w-11/12 h-14 rounded-2xl pl-4 text-black"
+                type="text"
+                id="comment"
+                name="comment"
+                placeholder="Add a comment..."
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+            />
+            <button type="submit" className="font-medium ml-2 text-black">post</button>
+        </form>
+    );
+}
