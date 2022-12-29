781e5562ecaf1e7a80a247228cf360f5b13110bc
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-29 10:36:58 +0200

    Add comments in postMessage model

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,14 +1,28 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const postSchema = mongoose.Schema({
     title: String,
-    descripton: String,
+    description: String,
+    type: String,
     ownerId: String,
     image: String,
     likes: {
         type: [String],
         default: []
     },
+    comments: [
+        {
+            comment: {
+                type: String,
+            },
+            created: {
+                type: Date, default: Date.now()
+            },
+            ownerId : {
+                type: String
+            }
+        }
+    ],
     createdAt: {
         type: Date,
         default: new Date()
