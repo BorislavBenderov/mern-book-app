ae8658ba268f2748cb0e0645d8eca22086f6fa97
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-02 12:28:07 +0200

    Add screens media query in tailwind config file

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,31 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        screens: {
+            '2xl': { 'max': '1535px' },
+            // => @media (max-width: 1535px) { ... }
+
+            'xl': { 'max': '1279px' },
+            // => @media (max-width: 1279px) { ... }
+
+            'lg': { 'max': '1050px' },
+            // => @media (max-width: 1050px) { ... }
+
+            'md': { 'max': '880px' },
+            // => @media (max-width: 880px) { ... }
+
+            'sm': { 'max': '775px' },
+            // => @media (max-width: 775px) { ... }
+
+            'vsm': { 'max': '455px' },
+            // => @media (max-width: 455px) { ... }
+        },
+        extend: {
+            gridTemplateColumns: {
+                // added new 4 column grid as new4
+                'auto': 'repeat(auto-fill, minmax(140px, 7fr))'
+            }
+        }
     },
     plugins: [],
 }
