0b64588ccafc141c53ae6070fd8ba3326273bfdb
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-02 12:31:59 +0200

    Make BookCard responsive design

--- client/src/components/book-card/BookCard.js
+++ client/src/components/book-card/BookCard.js
@@ -5,17 +5,17 @@ export const BookCard = ({ book }) => {
         <>
             <Link to={`/books/${book._id}`}>
                 <div className="">
-                    <div className="mb-2 overflow-hidden">
+                    <div className="mb-2 overflow-hidden h-60">
                         <img
                             src={book.image}
                             alt="book"
-                            className="rounded-2xl"
+                            className="rounded-2xl h-full"
                         />
                     </div>
                     <div className="">
                         <h3 className="text-xl font-semibold">{book.title}</h3>
                         <div className="">
-                            <span className="">{book.description}</span>
+                            <span className="">{book.type}</span>
                         </div>
                     </div>
                 </div>
